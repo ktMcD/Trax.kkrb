4cc9b00e8f8172a6ed6893477a3b26b009d7cb47
ktMcD <geek.katie@gmail.com>
2024-07-25 12:24:24 -0700

    Final Update to Consume PDF

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Windows.Documents;
 using System.Windows.Media.TextFormatting;
 using System.Windows.Xps.Packaging;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace Trax.kkrb
 {
@@ -21,6 +24,7 @@ namespace Trax.kkrb
         string tag2 = "The City of Sunshine's official radio station: Sunny 107; ";
         string tag1 = "Today's hits. Yesterday's favorites. Sunny 107; ";
         string tag3 = "Sunny 107: the Basin's soft rock station; ";
+        string tag4 = "Today's hits; Timeless favorites. Sunny 107;";
 
         [STAThread]
         private static void Main(string[] args)
@@ -33,7 +37,7 @@ namespace Trax.kkrb
         {
             bool keepTrying = true;
             string userResponse = "";
-            string exportedText = "";
+            string rawText = "";
 
             while (keepTrying)
             {
@@ -47,15 +51,15 @@ namespace Trax.kkrb
                 else
                 {
                     keepTrying = false;
-                    inFile = filePath + "KKRBLog_" + logDate.ToString("M-d-yy") + ".xps";
+                    inFile = filePath  + logDate.ToString("yyMMdd") + ".pdf";
                     outFile = filePath + "KKRBLog_" + logDate.ToString("M-d-yy") + ".txt";
                     script = filePath + "_ScriptLog_" + logDate.ToString("M-d-yy") + ".txt";
                 }
 
             }
-            exportedText = ConvertToText(inFile);
-            // Console.WriteLine(exportedText);
-            ProcessText(exportedText);
+            rawText = GetTextFromPDF();
+            //Console.WriteLine(rawText);
+            ProcessText();
         }
 
         private string GetLogDate()
@@ -64,168 +68,121 @@ namespace Trax.kkrb
             return Console.ReadLine();
         }
 
-        [STAThread]
-        private string ConvertToText(string fileName)
+        private string GetTextFromPDF()
         {
-            string fullPageText = "";
-            decimal originY = 0;
-            try
+            StringBuilder text = new StringBuilder();
+            using (PdfReader reader = new PdfReader(inFile))
             {
-                XpsDocument xpsDocument = new XpsDocument(fileName, System.IO.FileAccess.Read);
-                IXpsFixedDocumentSequenceReader fixedDocSeqReader = xpsDocument.FixedDocumentSequenceReader;
-                IXpsFixedDocumentReader document = fixedDocSeqReader.FixedDocuments[0];
-                FixedDocumentSequence sequence = xpsDocument.GetFixedDocumentSequence();
-
-                for (int pageCount = 0; pageCount < sequence.DocumentPaginator.PageCount; ++pageCount)
+                for (int i = 1; i <= reader.NumberOfPages; i++)
                 {
-                    IXpsFixedPageReader _page = document.FixedPages[pageCount];
-                    StringBuilder currentText = new StringBuilder();
-                    System.Xml.XmlReader pageContentReader = _page.XmlReader;
-
-                    if (pageContentReader != null)
-                    {
-                        while (pageContentReader.Read())
-                        {
-                            if (pageContentReader.Name == "Glyphs")
-                            {
-                                if (pageContentReader.HasAttributes)
-                                {
-                                    if (pageContentReader.GetAttribute("UnicodeString") != null &&
-                                       !pageContentReader.GetAttribute("UnicodeString").Contains("Input File") &&
-                                       !pageContentReader.GetAttribute("UnicodeString").Contains("KKRB FM") &&
-                                       !pageContentReader.GetAttribute("UnicodeString").Contains("Legal ID"))
-                                    {
-                                        if (decimal.Parse(pageContentReader.GetAttribute("OriginY")) != originY)
-                                        {
-                                            currentText.AppendLine();
-                                            originY = decimal.Parse(pageContentReader.GetAttribute("OriginY")); ;
-                                        }
-                                        currentText.Append(pageContentReader.GetAttribute("UnicodeString"));
-                                        currentText.Append("|");
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    fullPageText += currentText.ToString();
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
                 }
-                xpsDocument.Close();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("something went wrong.");
-                Console.WriteLine(e.Message);
-                Console.ReadKey();
-            }
-            File.WriteAllText(outFile, fullPageText);
-            return fullPageText;
+            File.WriteAllText(outFile, text.ToString());
+            return text.ToString();
         }
 
-        public void ProcessText(string convertedText)
+        public void ProcessText()
         {
-
-            int iHour = 0;
-            int iWheelPos = 0;
-            int iMinutePos = 0;
             Random rando = new Random();
+            int iHour = 0;
             string txtFile = outFile;
             StreamWriter writer = new StreamWriter(script, false);
+            writer.WriteLine("------------------------------------------------------------");
             writer.WriteLine($"KKRB Script for {logDate.ToString("ddd")}, {logDate.ToString("M-d-yy")}");
-            string whichTag = "";
+            writer.WriteLine("------------------------------------------------------------");
+            writer.WriteLine();
+            string whichTag;
+            string prevLine = "";
+            string thisLine = "";
 
-            // th
-            if (File.Exists(txtFile))
+            if (File.Exists(outFile))
             {
-                // Store each line in array of strings 
-                string[] lines = File.ReadAllLines(txtFile);
-                foreach (string line in lines)
+                string[] lines = File.ReadAllLines(outFile);
+                /* foreach is a PITA */
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    iHour = GetHour(line);
+                    string[] fields = lines[i].Split(' ');
+                    iHour = GetHour(lines[i]);
                     if ((iHour >= 6 && iHour < 18) &&
-                        !line.Contains("SWEEPER") &&
-                        !line.Contains("Artist") &&
-                        !line.Contains("WX") &&
-                        !line.Contains("|NEW|DA1000|"))
+                        !lines[i].Contains("SWEEPER") &&
+                        !lines[i].Contains("Artist") &&
+                        !lines[i].Contains("WX") &&
+                        !lines[i].Contains("Stock Market") &&
+                        fields[2] != "NEW")
                     {
-                        string[] fields = line.Split('|');
-                        if (line.Contains("DA1000"))
+                        thisLine = fields[2];
+                        if (lines[i].Contains("VOICETRACK"))
                         {
-                            Console.WriteLine($"");
-                            Console.WriteLine($"***************** {fields[0]} HOUR *****************");
+                            whichTag = SelectTag(rando.Next(7, 2000));
+                            writer.WriteLine();
+                            writer.WriteLine($"{fields[3]}");
+                            writer.WriteLine("----------------");
+                            writer.WriteLine($"{whichTag}");
+                            writer.WriteLine();
                         }
-                        // get voice track (jv0608, jn...//
-                        else if (line.ToLower().Contains("voice track") ||
-                                 line.ToLower().Contains("voicetrack"))
-                        {
-                            iWheelPos = GetWheelPosition(fields[3]);
-                            iMinutePos = GetMinutePosition(fields[3]);
-                            Console.WriteLine($"");
-                            Console.WriteLine($"{fields[3]}");
-                            Console.WriteLine($"----------------");
-                            if (iMinutePos != 20)
-                            {
-                                int randoNum = rando.Next();
-                                if (randoNum % 3 == 0)
-                                {
-                                    // "Today's hits. Yesterday's favorites. Sunny 107; "
-                                    whichTag = tag1;
-                                }
-                                else if (randoNum % 2 == 0)
-                                {
-                                    // "The City of Sunshine's official radio station: Sunny 107; "
-                                    whichTag = tag2;
-                                }
-                                else
-                                {
-                                    // "Sunny 107: the Basin's soft rock station; "
-                                    whichTag = tag3;
-                                }
-                                Console.WriteLine(whichTag);
-                            }
-                            else
-                            {
-                                Console.WriteLine(":20 BREAK TAGLINE HERE ");
-                            }
-                            Console.WriteLine();
-
-                        } // line contains voicetrack or voice track
                         else
                         {
-                            foreach (string field in fields)
+                            if (lines[i].Contains("STOPSET"))
                             {
-                                Console.Write($"{field} ");
+                                writer.WriteLine($"{fields[5]} {fields[6]}");
                             }
-                            Console.WriteLine() ;
+
+                            else if (prevLine == "CM" &&
+                                     thisLine != prevLine)
+                            {
+                                writer.WriteLine();
+                            }
+                            if (!lines[i].Contains("STOPSET"))
+                            {
+                                writer.WriteLine($"{lines[i]}");
+                            }
                         }
                     }
+                    prevLine = thisLine;                    
                 }
-                writer.Flush();
-                writer.Close();
-                writer.Dispose();
-                // Console.ReadLine();
-                /*
-                            if (File.Exists(outFile))
-                            {
-                                File.Delete(outFile);
-                            }
-                */
-                /*            if (File.Exists(inFile))
-                            {
-                                File.Delete(inFile);
-                            }
-                */
             }
+            writer.Flush();
+            writer.Close();
+            writer.Dispose();
+            Console.ReadLine();
         } // ProcessText
 
-        private int GetMinutePosition(string vtfield)
+        private string SelectTag(int randomTag)
+        {
+            
+            bool lIsPrime = IsPrime(randomTag);
+            if (lIsPrime)
+            {
+                return tag4;
+            }
+            if (randomTag % 7 == 0)
+            {
+                return tag1;
+            }
+            if (randomTag % 11 == 0)
+            {
+                return tag3;
+            }
+            return tag2;
+        }
+
+
+        private bool IsPrime(int whatNumber)
+        {
+            for (uint i = 8; i < whatNumber; i++)
+                if (whatNumber % i == 0) return false;
+            return true;
+        }
+
+        private int GetMinutePosition(string vtword)
         {
             int minutePos = 0;
             string subst = "";
-            if (vtfield != null &&
-                vtfield.Length > 0)
+            if (vtword != null &&
+                vtword.Length > 0)
             {
-                subst = vtfield.Substring(vtfield.Length - 2, 2);
+                subst = vtword.Substring(vtword.Length - 2, 2);
             }
 
             if (int.TryParse(subst, out minutePos))
@@ -235,14 +192,14 @@ namespace Trax.kkrb
             return 0;
         } // GetMinutePosition
 
-        private int GetWheelPosition(string vtfield)
+        private int GetWheelPosition(string vtword)
         {
             int wheelPos = 0;
             string subst = "";
-            if (vtfield != null &&
-                vtfield.Length > 0)
+            if (vtword != null &&
+                vtword.Length > 0)
             {
-                subst = vtfield.Substring(vtfield.Length - 4, 4);
+                subst = vtword.Substring(vtword.Length - 4, 4);
             }
 
             if (int.TryParse(subst, out wheelPos))
